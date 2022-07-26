e2f61d146e97530adc7390208dc240c42843ba5c
VadimZhukov <vadim.zhukov@gmail.com>
2022-07-26 13:21:42 +0300

    feat: configured timezone - Moscow, default locale - ru, added static pages /about and /about/author, basic entities in README

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,15 @@ module Testguru
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Moscow"
     # config.eager_load_paths << Rails.root.join("extras")
+    # Where the I18n library should search for translation files
+    I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
+
+    # Permitted locales available for the application
+    I18n.available_locales = [:en, :ru]
+
+    # Set default locale to something other than :en
+    I18n.default_locale = :ru
   end
 end
---
+++ config/initializers/version.rb
@@ -0,0 +1,3 @@
+module Testguru
+  VERSION = "1.0.0"
+end
