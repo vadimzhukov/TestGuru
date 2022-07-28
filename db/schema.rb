ee3594c8426a1ee53e830ce21f055bce1035e1c8
VadimZhukov <vadim.zhukov@gmail.com>
2022-07-28 09:53:47 +0300

    fix: remigrted new complex migrations to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_27_150830) do
+ActiveRecord::Schema.define(version: 2022_07_27_131116) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
