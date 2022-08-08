27e259cdbed983ffb41a37538dd8a153d49cff6a
VadimZhukov <vadim.zhukov@gmail.com>
2022-08-08 10:19:04 +0300

    fix: исправил ассоциацию has_many для User-Test, отрефакторил метод tests_by_level пуще некуда )

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  has_many :tests
+  has_many :creatives, class_name: "Test", inverse_of: :author
 
   has_many :test_of_users
   has_many :tests, through: :test_of_users
 
   def tests_by_level(level)
-    Test.joins(:test_of_users).where("tests.level = ? AND tu.user_id = ?", level, self.id)
+    self.tests.joins(:test_of_users).where(level: level)
   end
 end
