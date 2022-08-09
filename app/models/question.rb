8b06623789069dd054f9d18d283c6be1a29876d5
VadimZhukov <vadim.zhukov@gmail.com>
2022-08-10 00:22:27 +0300

    fix: изменил логику валидации ответов - теперь при создании ответа проверяется что указан соответствующий вопрос и что у вопроса уже есть не более 4 ответов, исправил скоуп для Test чтобы он не сортировал и выводил именно ActiveRecord::Relation, исправил составной индекс уникальности  для теста - вынес в миграцию

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,12 +4,6 @@ class Question < ApplicationRecord
   has_many :answers
 
   validates :body, presence: true
-  validate :validate_answers_count  
-
-  private
-
-  def validate_answers_count
-    errors.add(:answers) unless answers.to_a.count.between?(1, 4)
-  end
+  
 
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -14,10 +14,12 @@ class Test < ApplicationRecord
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
-  scope :tests_by_category, -> (category_name) { Test.joins(:category).
-                                                  where("categories.title" => category_name).
-                                                  order(title: :desc).
-                                                  pluck("tests.title") }
+  scope :by_category, -> (category_name) { Test.joins(:category).
+                                           where("categories.title" => category_name) }
 
   scope :tests_by_level, -> (level) { where("tests.level" => level) }
+
+  def tests_by_category(cat_name)
+    Test.by_category(cat_name).order(title: :desc).pluck("tests.title")
+  end
 end
