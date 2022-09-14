c9e470b35a84cd58718a03a514a235939883f742
VadimZhukov <vadim.zhukov@gmail.com>
2022-09-14 20:15:26 +0300

    fix: отработаны замечания codereview#2

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -1,5 +1,7 @@
 module FlashHelper
   def flash_messages(messages)
-    messages.map { |key, value| content_tag(:p, value.to_s, class: "flash #{key}") }.join('').html_safe
+    flashes_html = messages.map do |key, value|
+      content_tag(:p, value.to_s, class: "flash #{key}")
+    end.join('').html_safe
   end
 end
