fb34200d44f449a3292035b0d13107db50b5a3f5
VadimZhukov <vadim.zhukov@gmail.com>
2022-11-07 16:16:14 +0300

    fix: убрал общий if в обработке поля подтверждения паспорта

--- app/javascript/packs/utilities/input_check.js
+++ app/javascript/packs/utilities/input_check.js
@@ -4,19 +4,22 @@ document.addEventListener("turbolinks:load", function() {
   let passwordConfirmationErrorMessage = document.getElementById('validationPasswordFeedback');
 
 
-  if (passwordConfirmInput) {
-    passwordConfirmInput.addEventListener('input', () => {
-    let password = passwordInput.value
-    let passwordConfirmation = passwordConfirmInput.value
-   
-    if (passwordConfirmation != password && passwordConfirmation != '') {
-        passwordConfirmInput.classList.add('is-invalid')
-        passwordConfirmationErrorMessage.classList.remove('hide');
-      }
-      else {
-        passwordConfirmationErrorMessage.classList.add('hide');
-        passwordConfirmInput.classList.remove('is-invalid');
-      }
-    })
+  if (!passwordConfirmInput) {
+    return
   }
+
+  passwordConfirmInput.addEventListener('input', () => {
+  let password = passwordInput.value
+  let passwordConfirmation = passwordConfirmInput.value
+  
+  if (passwordConfirmation != password && passwordConfirmation != '') {
+      passwordConfirmInput.classList.add('is-invalid')
+      passwordConfirmationErrorMessage.classList.remove('hide');
+    }
+    else {
+      passwordConfirmationErrorMessage.classList.add('hide');
+      passwordConfirmInput.classList.remove('is-invalid');
+    }
+  })
+  
 })
