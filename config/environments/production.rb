8801ea40d9ccfe2b8c25ee3c3a6e043be2997236
VadimZhukov <vadim.zhukov@gmail.com>
2022-11-15 12:42:16 +0300

    fix: исправлена ошибка конфигурации user_name для action_mailer в production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    username: ENV['SMTP_USERNAME'],
+    user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
