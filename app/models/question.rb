10488667be852b988ad8e58b65152980d3d17bc8
VadimZhukov <vadim.zhukov@gmail.com>
2022-11-18 22:45:37 +0300

    fix: исправлена ошибка препятствующая удалению теста, который ранее был пройден кем-либо

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,8 +2,8 @@ class Question < ApplicationRecord
   belongs_to :test
 
   has_many :answers, dependent: :destroy
+  has_many :test_passages, foreign_key: :current_question, dependent: :destroy
   has_many :gists, dependent: :destroy
-  
 
   validates :body, presence: true
   validates :test_id, uniqueness: { scope: :body, message: 'Вопрос с таким текстом для данного теста уже существует' }
