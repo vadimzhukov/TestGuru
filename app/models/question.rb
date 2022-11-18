5434195bc3f0398f46926ef4b848beee391da12d
VadimZhukov <vadim.zhukov@gmail.com>
2022-11-19 00:52:03 +0300

    fix: добавлено поле questions_count для кэширования и уменьшения количества запросов к БД при рендеринге формы списка тестов

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ApplicationRecord
+  after_create :increase_questions_count_in_test
+  after_destroy :decrease_questions_count_in_test
+
   belongs_to :test
 
   has_many :answers, dependent: :destroy
@@ -7,4 +10,14 @@ class Question < ApplicationRecord
 
   validates :body, presence: true
   validates :test_id, uniqueness: { scope: :body, message: 'Вопрос с таким текстом для данного теста уже существует' }
+
+  private
+
+  def increase_questions_count_in_test
+    self.test.update_attribute('questions_count', self.test.questions_count += 1)
+  end
+
+  def decrease_questions_count_in_test
+    self.test.update_attribute('questions_count', self.test.questions_count -= 1) unless self.test.questions_count == 0
+  end
 end
